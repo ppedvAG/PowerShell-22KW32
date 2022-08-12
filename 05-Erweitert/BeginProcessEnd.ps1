5e60dd1b9709c1785fa51748c66b349d331acc3f
Stefan Ober <StefanO@ppedv.de>
2022-08-12 14:35:17 +0200

    Fort: Beginn Process End

---
+++ 05-Erweitert/BeginProcessEnd.ps1
@@ -0,0 +1,23 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName=$true)]
+[string]$Name,
+
+[Parameter(ValueFromPipeLineByPropertyName=$true)]
+[int]$PM
+
+)
+
+Begin
+{
+    $Color = Get-Random -Minimum 0 -Maximum 15
+}
+Process
+{
+$Ausgabe = $Name + " | "  + ("{0:N2}" -f ($PM / 1MB))
+Write-Host -Object $Ausgabe -ForegroundColor $color
+}
+End
+{
+Write-Host -Object "-- Ende Der Übermittlung --" -ForegroundColor $color
+}
